c6c22edd2420619ddd14cb6afc0c354bd08ec3e8
JevinLeon <jevinleon@gmail.com>
2024-05-21 18:19:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const express = require("express");
+const router = require("./routes");
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api", router);
+
+app.use("*", (req, res) => {
+  res.status(404).json({
+    data: null,
+    message: "Route not found!",
+  });
+});
+
+// Error Middleware
+app.use((err, req, res, next) => {
+  let statusCode = 500;
+  let message = "Internal Server Error";
+
+  if (err.statusCode) {
+    statusCode = err.statusCode;
+  }
+  if (err.message) {
+    message = err.message;
+  }
+
+  res.status(statusCode).json({
+    data: null,
+    message,
+  });
+});
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
