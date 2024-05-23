7dfff493c0cca419682ea3a2d750f2533fedc402
dvlboo <kuhhhckr@gmail.com>
2024-05-23 14:00:19 +0700

    auth : register

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USERNAME,
     pass: process.env.EMAIL_PASSWORD
