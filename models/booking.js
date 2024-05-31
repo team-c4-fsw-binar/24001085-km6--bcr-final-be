0dccff82be202a661760bd64a7fff3399c3e7396
JevinLeon <jevinleon@gmail.com>
2024-05-31 19:05:48 +0700

    deploy

--- models/booking.js
+++ models/booking.js
@@ -1,26 +1,28 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Booking extends Model {
-    
     static associate(models) {
-      Booking.belongsTo(models.User, { foreignKey : 'user_id' })
-      Booking.hasOne(models.Payment, { foreignKey : 'booking_id' })
-      Booking.belongsTo(models.Flight, { foreignKey : 'flight_id'})
-      Booking.hasMany(models.Seat, { foreignKey : 'booking_id' })
-      Booking.hasOne(models.BookingPessanger, { foreignKey : 'booking_id'})
+      Booking.belongsTo(models.User, { foreignKey: "user_id" });
+      Booking.hasOne(models.Payment, { foreignKey: "booking_id" });
+      Booking.belongsTo(models.Flight, { foreignKey: "flight_id" });
+      Booking.hasMany(models.Seat, { foreignKey: "booking_id" });
+      Booking.hasOne(models.BookingPassenger, { foreignKey: "booking_id" });
     }
   }
-  Booking.init({
-    user_id: DataTypes.INTEGER,
-    flight_id: DataTypes.INTEGER,
-    passenger_id: DataTypes.INTEGER,
-    orderDate: DataTypes.DATE,
-    priceAmount: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Booking',
-    paranoid: true
-  });
+  Booking.init(
+    {
+      user_id: DataTypes.INTEGER,
+      flight_id: DataTypes.INTEGER,
+      passenger_id: DataTypes.INTEGER,
+      orderDate: DataTypes.DATE,
+      priceAmount: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Booking",
+      paranoid: true,
+    }
+  );
   return Booking;
 };
