e67877bebe5b5ab624872941f3e9d93bd1e8e199
Jevin Leon <82362442+JevinLeon@users.noreply.github.com>
2024-06-17 20:48:21 +0700

    Merged and added status update for payment (#20)

--- models/payment.js
+++ models/payment.js
@@ -3,7 +3,10 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Payment extends Model {
     static associate(models) {
-      Payment.belongsTo(models.Booking, { foreignKey: "booking_code" });
+      Payment.belongsTo(models.Booking, {
+        foreignKey: "booking_code",
+        targetKey: "code",
+      });
     }
   }
   Payment.init(
