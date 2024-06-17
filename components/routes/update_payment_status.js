ecb969ff1145ffb7e4d9036fc8dc7970c6772498
JevinLeon <jevinleon@gmail.com>
2024-06-17 21:10:35 +0700

    deploy with cron

--- components/routes/update_payment_status.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-const { updatePaymentStatus } = require("../controllers/update_payment_status");
-
-router.route("/").put(updatePaymentStatus);
-
-module.exports = router;
