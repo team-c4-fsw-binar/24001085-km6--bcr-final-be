8ba0cdfb34d532d51dda78f6b8921c8f10855b92
Hasan Nur Wakhid <118034780+hasannurwakhid@users.noreply.github.com>
2024-06-28 15:34:01 +0700

    Add new filter booking history (#32)
    * cek
    
    * add airpor name filter

--- components/services/bookingHistory.js
+++ components/services/bookingHistory.js
@@ -4,7 +4,7 @@ exports.getBookingHistoriesByFlightIDandDateRange = async (
   user_id,
   payload
 ) => {
-  const { code, startDate, endDate } = payload;
+  const { code, startDate, endDate, airportName } = payload;
 
   const data = await getBookingsByUserId(user_id);
 
@@ -26,6 +26,26 @@ exports.getBookingHistoriesByFlightIDandDateRange = async (
     filteredData = filteredData.filter((booking) => {
       const bookingDate = new Date(booking.order_date);
       return bookingDate >= start && bookingDate <= end;
+    });
+  }
+
+  if (airportName) {
+    filteredData = filteredData.filter((booking) => {
+      let departureFlightDepartureAirport =
+        booking.departureFlight_respon?.departureAirport_respon?.name?.toLowerCase();
+      let departureFlightArrivalAirport =
+        booking.departureFlight_respon?.arrivalAirport_respon?.name?.toLowerCase();
+      let returnFlightDepartureAirport =
+        booking.returnFlight_respon?.departureAirport_respon?.name?.toLowerCase();
+      let returnFlightArrivalAirport =
+        booking.returnFlight_respon?.arrivalAirport_respon?.name?.toLowerCase();
+
+      return (
+        departureFlightDepartureAirport?.includes(airportName.toLowerCase()) ||
+        departureFlightArrivalAirport?.includes(airportName.toLowerCase()) ||
+        returnFlightDepartureAirport?.includes(airportName.toLowerCase()) ||
+        returnFlightArrivalAirport?.includes(airportName.toLowerCase())
+      );
     });
   }
 
